dad01f431df20c71e92f6bd25be5dbeda7477a56
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2025-03-12 04:39:28 +0000

    update axios url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "https://moneo-fullstack-backend.onrender.com"
+const API_URL = "https://moneo-fullstack-backend.onrender.com/api"
 
 const api = axios.create({
     baseURL: API_URL,
